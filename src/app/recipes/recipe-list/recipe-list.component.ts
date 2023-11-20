2185379f337c0c64060b3890003cc1c600a1ff73
Tobias Prima <primatjahja@gmail.com>
2023-11-20 13:45:21 +0800

    add recipe list component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+}
