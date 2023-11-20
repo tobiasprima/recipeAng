03b232b6ccbb480745d76ec4dad67057e72d8c61
Tobias Prima <primatjahja@gmail.com>
2023-11-20 13:43:23 +0800

    create recipes component

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipes.component.html',
+  styleUrl: './recipes.component.css'
+})
+export class RecipesComponent {
+
+}
