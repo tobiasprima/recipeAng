7c976681dbba39a2447416a95a93d3696710baa6
Tobias Prima <primatjahja@gmail.com>
2023-11-20 13:47:10 +0800

    add recipe components

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-detail.component.html',
+  styleUrl: './recipe-detail.component.css'
+})
+export class RecipeDetailComponent {
+
+}
---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-item.component.html',
+  styleUrl: './recipe-item.component.css'
+})
+export class RecipeItemComponent {
+
+}
