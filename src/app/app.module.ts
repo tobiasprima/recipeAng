f83b5ddd5137726356abab4d0552c960f6d3dde4
Tobias Prima <primatjahja@gmail.com>
2023-11-20 13:04:49 +0800

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule
   ]
 })
 export class AppModule { }
