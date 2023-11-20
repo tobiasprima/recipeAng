2a1541b065c4db0420359016b2e647bfbee0f181
Tobias Prima <primatjahja@gmail.com>
2023-11-20 13:51:11 +0800

    add components

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shopping-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-edit.component.html',
+  styleUrl: './shopping-edit.component.css'
+})
+export class ShoppingEditComponent {
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shopping-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent {
+
+}
