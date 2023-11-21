91d193ee343c2c322cf1884ba7f2ea74443bfb72
Tobias Prima <primatjahja@gmail.com>
2023-11-21 14:11:30 +0800

    add navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>()
+    @Output() featureSelected = new EventEmitter<string>();
     
     onSelect(feature: string){
         this.featureSelected.emit(feature);
