2f934cede66c79a2eeccd867b4b852444bc79d2b
Tobias Prima <primatjahja@gmail.com>
2023-11-22 23:18:12 +0800

    add recipe start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { Component, NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router'
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 const appRoute: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent},
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipeStartComponent}
+    ]},
     { path: 'shopping-list', component: ShoppingListComponent}
 ]
 
