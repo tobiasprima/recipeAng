11dee9e749752934391b6134da3126d1a8480175
Tobias Prima <primatjahja@gmail.com>
2023-11-22 15:08:23 +0800

    add ingredient in recipe description

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable()
 
@@ -11,12 +12,18 @@ export class RecipeService {
           'A Test Recipe', 
           'This is a test', 
           'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg',
-          []),
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 1)
+          ]),
         new Recipe(
           'Another Test Recipe', 
           'This is a test', 
           'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg',
-          []),
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 5),
+          ]),
       ];
     
       getRecipe(){
