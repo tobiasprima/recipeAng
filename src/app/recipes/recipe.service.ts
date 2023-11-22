15a0c20caf83e7e6e3710c71828e3159db893267
Tobias Prima <primatjahja@gmail.com>
2023-11-22 15:02:55 +0800

    add ingredient property

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,8 +7,16 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg'),
-        new Recipe('Another Test Recipe', 'This is a test', 'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg'),
+        new Recipe(
+          'A Test Recipe', 
+          'This is a test', 
+          'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg',
+          []),
+        new Recipe(
+          'Another Test Recipe', 
+          'This is a test', 
+          'https://img.taste.com.au/h2qVkJjl/taste/2021/09/air-fryer-chicken-rissoles-recipe-173558-1.jpg',
+          []),
       ];
     
       getRecipe(){
