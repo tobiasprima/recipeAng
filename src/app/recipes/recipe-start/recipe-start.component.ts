2c21aeae0272693b7777e67e0a547a74056f60b5
Tobias Prima <primatjahja@gmail.com>
2023-11-22 23:14:54 +0800

    add recipe-start component

---
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-start',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-start.component.html',
+  styleUrl: './recipe-start.component.css'
+})
+export class RecipeStartComponent {
+
+}
