84872cde52632d60580c3f704e21476782392519
Tobias Prima <primatjahja@gmail.com>
2023-11-26 02:37:32 +0800

    add delete and clear method

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
 
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit{
   recipeForm!: FormGroup;
 
   constructor(private route: ActivatedRoute,
-    private recipeService: RecipeService){}
+    private recipeService: RecipeService,
+    private router: Router){}
 
   ngOnInit() {
       this.route.params
@@ -39,6 +40,7 @@ export class RecipeEditComponent implements OnInit{
     } else{
       this.recipeService.addRecipe(this.recipeForm.value)
     }
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -50,6 +52,10 @@ export class RecipeEditComponent implements OnInit{
         Validators.pattern(/^[1-9]+[0-9]*$/)] ),
     })
    )
+  }
+
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 
   private initForm(){
