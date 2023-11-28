c0ee9e244b687036645709b7d99a34ec0998cec1
Tobias Prima <primatjahja@gmail.com>
2023-11-28 23:01:57 +0800

    protection for ingredients if none given

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
@@ -20,6 +21,11 @@ export class DataStorageService {
     fetchRecipes(){
         this.httpClient.get<Recipe[]>(
             "https://ng-course-recipe-book-8989c-default-rtdb.firebaseio.com/recipes.json")
+            .pipe(map(recipes=> {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []};
+                })
+            }))
             .subscribe(recipes => {
                 this.recipeService.setRecipes(recipes);
             })
