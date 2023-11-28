9e9b8b1570eeb0d409ebf0d8fab103d12e406a14
Tobias Prima <primatjahja@gmail.com>
2023-11-28 18:01:35 +0800

    add service to template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component ( {
     selector: 'app-header',
@@ -8,4 +9,10 @@ import { Component } from "@angular/core";
 
 export class HeaderComponent {
 
+    constructor(private dataStorage: DataStorageService){}
+
+
+    onSaveData(){
+        this.dataStorage.storeRecipes()
+    }
 }
