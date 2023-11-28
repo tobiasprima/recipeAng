b217ee22a320ac7eabd66d557214f248dac6d122
Tobias Prima <primatjahja@gmail.com>
2023-11-28 17:52:46 +0800

    create service to store recipes

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({providedIn: "root"})
+export class DataStorageService {
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipe();
+        this.httpClient.put("https://ng-course-recipe-book-8989c-default-rtdb.firebaseio.com.json", recipes)
+    }
+}
