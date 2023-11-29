639b22987077f28035c6b5aa8069704775ca38ec
Tobias Prima <primatjahja@gmail.com>
2023-11-29 13:39:41 +0800

    add submit button

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,29 +8,28 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 export class AuthComponent implements OnInit{
     isLoginMode = true;
-
-
-    constructor(){}
     authForm!: FormGroup;
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode
     }
 
+    constructor(){}
 
     onSubmit(){
-        
+        console.log(this.authForm);
     }
     
     ngOnInit() {
+
         this.authForm = new FormGroup({   
             'email' : new FormControl(null, [Validators.required, Validators.email]),
             'password' : new FormControl(null, [Validators.required, Validators.minLength(6)])
         })
+      
+        
     }
     private initForm(){
 
-
-      
     }
 }
