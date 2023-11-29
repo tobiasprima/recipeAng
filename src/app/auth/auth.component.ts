e91b0dcc6885ddb42066fc984877a24c30e275f3
Tobias Prima <primatjahja@gmail.com>
2023-11-29 13:28:18 +0800

    create form group

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
 
-export class AuthComponent{
+export class AuthComponent implements OnInit{
     isLoginMode = true;
+
+
+    constructor(){}
+    signUpForm!: FormGroup;
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode
+    }
+
+
+    ngOnInit(): void {
+       
+    }
+
+    onSignUp(){
+        
+    }
+    
+    private initForm(){
+
+
+        this.signUpForm = new FormGroup({   
+            'email' : new FormControl(null, [Validators.required, Validators.email]),
+            'password' : new FormControl(null, [Validators.required, Validators.minLength(6)])
+        })
     }
 }
