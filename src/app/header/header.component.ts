f75b21baae3c9407d8791e30200c48fe5609afdd
Tobias Prima <primatjahja@gmail.com>
2023-11-30 14:33:44 +0800

    add logout method

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -31,6 +31,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.dataStorage.fetchRecipes().subscribe();
     }
 
+    onLogout(){
+        this.authService.logout();
+    }
+
     ngOnDestroy(): void {
         this.userSub.unsubscribe();
     }
