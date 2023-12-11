fbe8e6039d7837ecba12efd94d1fe7448fe927c7
Tobias Prima <primatjahja@gmail.com>
2023-12-11 13:10:10 +0800

    Split to feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router'
-import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeResolverService } from "./recipes/recipe-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
-import { authGuardGuard } from "./auth/auth.guard";
 
 const appRoute: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent, canActivate: [authGuardGuard] ,children: [
-        { path: '', component: RecipeStartComponent},
-        { path: 'new', component: RecipeEditComponent},
-        { 
-            path: ':id', 
-            component: RecipeDetailComponent, 
-            resolve:
-            {
-                routeResolver: RecipeResolverService
-            }
-            
-        },
-        { path: ':id/edit', 
-        component: RecipeEditComponent,  
-        resolve:{
-            routeResolver:  RecipeResolverService
-        }
-    },
-]},
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'}, 
 { path: 'shopping-list', component: ShoppingListComponent},
 { path: 'auth', component: AuthComponent},
 
