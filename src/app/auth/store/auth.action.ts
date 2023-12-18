20f5c5219155273ece7b2b06ac2013e2c85e16db
Tobias Prima <primatjahja@gmail.com>
2023-12-18 16:38:46 +0800

    implement login through effects

--- src/app/auth/store/auth.action.ts
+++ src/app/auth/store/auth.action.ts
@@ -1,5 +1,6 @@
 import { Action } from "@ngrx/store";
 
+export const LOGIN_START = 'LOGIN_START'
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
@@ -17,6 +18,14 @@ export class Login implements Action{
 
 export class Logout implements Action{
     readonly type = LOGOUT;
+}
+
+export class LoginStart implements Action{
+    readonly type = LOGIN_START;
+    constructor(public payload:{
+        email: string,
+        password: string,
+    }){}
 }
 
 export type AuthAction = Login | Logout;
