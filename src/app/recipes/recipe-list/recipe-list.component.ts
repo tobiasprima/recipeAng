ea423aa6a2b39a29085a70cda86434cb99c0ddd2
Tobias Prima <primatjahja@gmail.com>
2023-12-19 17:12:55 +0800

    feat: select actions in component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, map } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,13 +16,18 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes!: Recipe[] ;
   subscription!: Subscription;
 
-  constructor(private recipeService: RecipeService,
+  constructor(
+    // private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute){}
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>
+    ){}
 
   ngOnInit() {
-      this.recipes=this.recipeService.getRecipe();
-      this.subscription = this.recipeService.recipesChanged
+      // this.recipes=this.recipeService.getRecipe();
+      // this.subscription = this.recipeService.recipesChanged
+      this.subscription = this.store.select('recipes')
+      .pipe(map(recipeState=> recipeState.recipes))
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,15 +1,19 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import { exhaustMap, map, take, tap } from "rxjs";
-import { AuthService } from "../auth/auth.service";
 import { Store } from "@ngrx/store";
 import * as fromApp from "../store/app.reducer";
+import * as RecipesActions from "../recipes/store/recipe.action";
 
 @Injectable({providedIn: "root"})
 export class DataStorageService {
-    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService, private store: Store<fromApp.AppState>){}
+    constructor(
+        private httpClient: HttpClient, 
+        private recipeService: RecipeService, 
+        // private authService: AuthService, 
+        private store: Store<fromApp.AppState>){}
 
     storeRecipes(){
         const recipes = this.recipeService.getRecipe();
@@ -37,7 +41,8 @@ export class DataStorageService {
                     })
                 }),
                 tap(recipes=> {
-                    this.recipeService.setRecipes(recipes);
+                    // this.recipeService.setRecipes(recipes);
+                    this.store.dispatch(new RecipesActions.SetRecipes(recipes));
                 }))
         }), )
     }
