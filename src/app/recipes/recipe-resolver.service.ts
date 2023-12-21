7956efcafadd951d1ebfcdfefdaaaacadbe4cb23
Tobias Prima <primatjahja@gmail.com>
2023-12-21 12:44:59 +0800

    feat: update resolver

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -3,15 +3,27 @@ import { ResolveFn } from "@angular/router";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Recipe } from "./recipe.model";
 import { RecipeService } from "./recipe.service";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import *as RecipeActions from "./store/recipe.action";
+import { Actions, ofType } from "@ngrx/effects";
+import { take } from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class RecipeResolverService   {
-    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService){}
+    constructor(
+        // private dataStorageService: DataStorageService, 
+        private recipeService: RecipeService,
+        private store: Store<fromApp.AppState>,
+        private actions$: Actions
+        ){}
 
     resolve: ResolveFn<Recipe[]> = () => {
         const recipes = this.recipeService.getRecipe();
         if(recipes.length === 0){
-            return this.dataStorageService.fetchRecipes();
+            // return this.dataStorageService.fetchRecipes();
+            this.store.dispatch(new RecipeActions.FetchRecipes());
+            return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1))
         } else {
             return recipes;
         }
