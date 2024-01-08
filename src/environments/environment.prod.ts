a95fa6aa72efaa0e3a7e690f6eedb7e04b45ec9f
Tobias Prima <88434271+tobiasprima@users.noreply.github.com>
2024-01-08 05:01:31 -0800

    Delete src/environments directory
    delete environments from github

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    firebaseAPIKey: 'AIzaSyBD34rWC15zWVIg9DNp9LXJvm8dlSERHLk'
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    firebaseAPIKey: 'AIzaSyBD34rWC15zWVIg9DNp9LXJvm8dlSERHLk'
-}
